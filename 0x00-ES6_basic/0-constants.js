7f8eb6996de97e84ade60a4a50cb678e184e72e5
“TijesuRejoice” <tijesuadeleke94@gmail.com>
2023-06-29 05:34:10 -0700

    Task one

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,2 +1,15 @@
 export function taskFirst() {
-	const task = 
+  var task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  var combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
