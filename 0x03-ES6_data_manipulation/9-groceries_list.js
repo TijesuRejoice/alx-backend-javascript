5a7a1c82d7a06b8ed46cf8994d68d7706390064d
“TijesuRejoice” <tijesuadeleke94@gmail.com>
2023-07-04 01:18:36 -0700

    MAin tasks

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+const groceriesList = () => {
+  const groceryMap = new Map();
+  groceryMap.set('Apples', 5);
+  groceryMap.set('Bananas', 3);
+  groceryMap.set('Milk', 1);
+  groceryMap.set('Bread', 2);
+  groceryMap.set('Eggs', 6);
+
+  return groceryMap;
+};
+
+
+export default groceriesList;
