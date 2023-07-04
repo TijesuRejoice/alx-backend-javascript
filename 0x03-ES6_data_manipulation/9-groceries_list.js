e7d1c4d6adad8a9a656c577bccb809dd2217c31e
“TijesuRejoice” <tijesuadeleke94@gmail.com>
2023-07-04 02:59:33 -0700

    Tasks more

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,13 +1,16 @@
 const groceriesList = () => {
-  const groceryMap = new Map();
-  groceryMap.set('Apples', 5);
-  groceryMap.set('Bananas', 3);
-  groceryMap.set('Milk', 1);
-  groceryMap.set('Bread', 2);
-  groceryMap.set('Eggs', 6);
-
-  return groceryMap;
+  const buffer = new Map();
+  const items = {
+    Apples: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice: 1,
+    Banana: 5,
+  };
+  for (const key of Object.keys(items)) {
+    buffer.set(key, items[key]);
+  }
+  return buffer;
 };
-
 
 export default groceriesList;
